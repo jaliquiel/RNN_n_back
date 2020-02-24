ce4f5ccbaadb61a10b274a3e89bdcd448349dc7d
jaliquiel <jaliquiel@gmail.com>
2020-02-24 02:22:31 -0500

    it runs but starts really high loss. Before changing w from 6,0 to 6,1

--- homework5_this_one_works.py
+++ homework5_this_one_works.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize  # For check_grad, approx_fprime
 import math
+from scipy.optimize import check_grad
 
 np.random.seed(1234)
 
@@ -25,41 +26,54 @@ class RNN:
         self.U = np.random.randn(numHidden, numHidden) * 1e-1
         self.V = np.random.randn(numHidden, numInput) * 1e-1
         self.w = np.random.randn(numHidden) * 1e-1
-        self.hs = {0:np.zeros((numHidden, numInput))}
-        self.yhats = {0:0}
+        self.hs = [np.zeros((numHidden, numInput))]
         # TODO: IMPLEMENT ME
 
-    def backward (self, y):
+    def backward (self,x, y):
         # TODO: IMPLEMENT ME
-        T = len(y)
-
-        gradient_MSE = (self.yhats[T] - y[T-1])
+        yhat = self.forward(x)
+        T = len(y) # = 50
+        # print(self.yhats.keys())
+        gradient_MSE = yhat[-1] - y[-1] # y = pos(50) - pos(49)
 
         #init gradient values
-        gradient_w = np.dot(gradient_MSE, np.transpose(self.hs[T]))
-        gradient_U = np.zeros(self.U.shape)
-        gradient_V = np.zeros(self.V.shape)
+        # gradient_w = np.zeros(self.w.shape).reshape(-1,1)
+        # gradient_U = np.zeros(self.U.shape)
+        # gradient_V = np.zeros(self.V.shape)
+
+        gradient_w = 0
+        gradient_U = 0
+        gradient_V = 0
 
 
-        gradient_h = np.dot(np.transpose(gradient_w), gradient_MSE)
+        gradient_h = np.dot(gradient_w, gradient_MSE).reshape(-1,1)
         
-
         for t in np.arange(T)[::-1]:
-            gradient_h = (gradient_h * (1 - self.hs[t + 1] ** 2))
+            # gradient_MSE = (yhat[t] - y[t])
+            # print("temp stuff")
+            # print(gradient_h.shape)
+            # temp = (gradient_h * (1 - self.hs[t+1] ** 2))
+            # print(temp.shape)
 
+            # print(f"gradient_h before is {gradient_h.shape}")
+            temp = np.dot(gradient_h,np.diag(1 - self.hs[t+1] ** 2))
+            # print(temp.shape)
 
-            gradient_w += np.dot(gradient_MSE, np.transpose(self.hs[t])) # could be self.hs[t-1]
-            gradient_U += gradient_h.dot(np.transpose(self.hs[t]))
-            gradient_V += gradient_h.dot(np.transpose(self.last_x[t]))
+            gradient_w += np.dot(gradient_MSE, self.hs[t+1].T).reshape(-1,1) # could be self.hs[t-1]
 
-            gradient_MSE = (self.yhats[T] - y[T-1])
+            # print("gradien U")
+            # print(temp.shape)
+            gradient_U += temp.reshape(-1,1).dot(np.transpose(self.hs[t]))
+            # gradient_U += 
+            gradient_V += temp.reshape(-1,1).dot(np.transpose(self.last_x[t]))
 
-
-            gradient_h = self.U.dot(gradient_h)
-
+            gradient_MSE = np.array(yhat[t-1] - y[t-1])
+            gradient_h = self.U.dot(temp).reshape(-1,1) #+  np.dot(np.transpose(gradient_w), gradient_MSE)
+            # print(f"gradient_h is {gradient_h.shape}")
+        
         # try wth out clip and see if it works!!!! study a little more the aspects of the algo.
-        for gradint in [gradient_V, gradient_U, gradient_w]:
-            np.clip(gradint, -1, 1, out=gradint)
+        # for gradint in [gradient_V, gradient_U, gradient_w]:
+        #     np.clip(gradint, -1, 1, out=gradint)
 
         return gradient_V, gradient_U, gradient_w
 
@@ -67,67 +81,44 @@ class RNN:
         # TODO: IMPLEMENT ME
 
         self.last_x = x
-        h_t = np.zeros((numHidden, numInput))
-        yhat_t = 0
+        self.hs = [np.zeros((numHidden, numInput))]
+        yhats = []
 
-        for i, x in enumerate(x, 1):
-            # print("my Vdotx shape is ")
-            # print(self.V.dot(x).shape)
-
-            # print("my udoths is ")
-            # print(self.U.dot(self.hs[i-1]).shape)
-
-            # print("my sum shape is")
-            # print((self.V.dot(x.reshape(1,-1)) + self.U.dot(self.hs[i-1])).shape)
-
-            h_t = np.tanh(self.V.dot(x.reshape(1,-1)) + self.U.dot(self.hs[i-1]))
-            print(f"my h_t shape is {h_t.shape}")
+        for i in range(len(x)):
+            h_t = np.tanh(self.V.dot(x[i]) + self.U.dot(self.hs[i]))
             yhat_t = self.w.dot(h_t)
-            self.hs[i] = h_t
-            print(type(yhat_t))
-            print(yhat_t.shape)
-            print(yhat_t)
-            self.yhats[i] = float(yhat_t) 
+            self.hs.append(h_t)
+            yhats.append(yhat_t)
 
         # return last saved yhat & h time step 
-        return h_t, yhat_t 
+        return yhats 
 
-    def SGD(self,X, y, batch_size, epochs, epsilon):
+    def SGD(self,x, y, batch_size, epochs, epsilon):
         # randomize training set
-        # self.SGD_param = [batch_size, epochs, epsilon, alpha]
-        permute = np.random.permutation(X.shape[1])
-        shuffled_X  = X.T[permute].T #(1, 50)
-        # shuffled_y = self.y.T[permute].T #(10,55000)
-        shuffled_y = y.T[permute].T
 
-        sample_size = X.shape[0] # total batch size
+        rate1 = 1e-1
+        rate2 = 1e-5
+        rate3 = 1e-7
 
-        # get all indexes based on batch size
-        rounds = get_indexes(sample_size, batch_size) # list of (start,end) for slicing each batch X
-
-        print(rounds)
 
         # start iteration loop
         for epoch in range(1, epochs+1):
             # print(f"Epoch [{epoch}]")
-            for indexes in rounds:
-                start, finish = indexes
-                self.forward(shuffled_X[:,start:finish])
-                gradient_V, gradient_U, gradient_w = self.backward(shuffled_y[:,start:finish])
-                self.V -= epsilon * gradient_V
-                self.U -= epsilon * gradient_U
-                self.w -= epsilon * gradient_w
+            gradient_V, gradient_U, gradient_w = self.backward(x, y)
+            # self.V -= rate3 * gradient_V
+            # self.U -= rate2 * gradient_U
+            # self.w -= rate1 * gradient_w.reshape(-1)
+            self.V -= epsilon * gradient_V
+            self.U -= epsilon * gradient_U
+            self.w -= epsilon * gradient_w.reshape(-1)
 
-            yhat = self.foward(X)
+            yhat = self.forward(x)
             MSE_val = MSE(yhat, y)
             print("This is epoch round [{}]".format(epoch))
-            print("Epoch [{}], MSE Loss: {}".format(epoch , MSE_valE))
+            print("Epoch [{}], MSE Loss: {}".format(epoch , MSE_val))
 
 def MSE (yhat, y):
-    coeff = 1 / (2 * X_tilde.shape[1])
-    sum = np.sum((yhat - y)**2)
-    mse = coeff * sum
-    return mse
+    return 0.5 * np.sum((yhat - y)**2)
 
 # From https://medium.com/@erikhallstrm/hello-world-rnn-83cd7105b767
 def generateData ():
@@ -140,21 +131,25 @@ def generateData ():
     y = list(y)
     return (x, y)
 
+
+
 if __name__ == "__main__":
     xs, ys = generateData()
-    # print(xs)
-    # print(ys)
     numHidden = 6
     numInput = 1
     numTimesteps = len(xs)
-    print(numTimesteps)
     rnn = RNN(numHidden, numInput, 1)
 
     batch_size = 1
-    epsilon = 0.01
-    epochs = 10
+    epsilon = 0.1
+    epochs = 1000000
 
-    rnn.SGD(np.array(xs).reshape(1,-1), np.array(ys).reshape(1,-1), batch_size, epochs,epsilon)
+    # a= check_grad(forward,backward,y)
+    # print(a)
+    rnn.SGD(xs, np.array(ys), batch_size, epochs,epsilon)
+
+
+
 
     # rnn2 = rnn.forward(np.array(xs).reshape(1,-1))
     # print(rnn.backward(ys))
